5fc459015d48d0d35c99ed876f94dcc0e895a3e1
cloud866 <dklein927@gmail.com>
2023-01-05 19:34:10 +0200

    Add Zombie

---
+++ задачки/Zombie/src/Zombie.java
@@ -0,0 +1,17 @@
+public class Zombie extends Monster {
+    //Fix this class
+    public Zombie(String name) {
+        super(name + " the Zombie", 5);
+    }
+
+    public void growl(){
+        System.out.println("Raaaauuughhhh");
+    }
+
+    @Override
+    public void attack(Monster monster){
+        monster.damage(getForce());
+        growl();
+    }
+
+}
