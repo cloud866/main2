19fea1f591dd524196ff4501bf2cd93d89b6539a
cloud866 <dklein927@gmail.com>
2023-01-05 19:23:23 +0200

    Add changes

--- задачки/CarMod5/src/Main.java
+++ задачки/CarMod5/src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 6;
-        int n = 6;
-        int n = 6;
-        int n = 6;
-
         System.out.println("Hello world!");
     }
 }
--- задачки/untitled155/src/Main.java
+++ задачки/untitled155/src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 6;
-        int n = 6;
-        int n = 6;
-        int n = 6;
+        int t = 7;
+        int t = 7;
+        int t = 7;
+
 
         System.out.println("Hello world!");
     }
