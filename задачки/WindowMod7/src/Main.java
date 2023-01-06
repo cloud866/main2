b1614efc67c480dd14ed523b225b4d702529972f
cloud866 <dklein927@gmail.com>
2023-01-06 15:36:38 +0200

    Add window

---
+++ задачки/WindowMod7/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+public class Main {
+    public static void main(String[] args) {
+        // создаем окно
+        JFrame jf = new JFrame();
+        // устанавливаем ему размеры
+        jf.setSize(400, 300);
+        jf.addMouseListener(new SimpleClickListener());
+        // и показываем его
+        jf.setVisible(true);
+
+    }
+}
