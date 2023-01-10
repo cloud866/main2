0dbcbcfd4c26dbf933ca8c4f7e6cefdfee729569
cloud866 <dklein927@gmail.com>
2023-01-10 11:51:08 +0200

    add sum

---
+++ задачки/MatrixMod6/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+       Matrix matrix = new Matrix(2, 3);
+        //System.out.println(matrix.getH());
+        //System.out.println(matrix.getW());
+        //System.out.println(Arrays.deepToString(matrix.getMatrix()));
+        //matrix.pprint();
+
+        Matrix m = new Matrix(new int[][]
+        {
+                {1,2,3},
+                {4,5,6}
+        });
+
+        matrix.sum(-4);
+        matrix.pprint();
+
+//        m.pprint();
+//        System.out.println(m.getH());  //2
+//        System.out.println(m.getW());  //3
+    }
+}
---
+++ задачки/MatrixMod6/src/Matrix.java
@@ -0,0 +1,59 @@
+public class Matrix {
+    private int[][] matrix;
+    private int w;
+    private int h;
+
+    public Matrix(int w, int h) {
+        this.w = w;
+        this.h = h;
+
+        this.matrix = new int[h][w];
+    }
+
+    public Matrix(int[][] matrix) {
+        this.h = matrix.length;
+        this.w = matrix[0].length;
+        this.matrix = matrix;
+    }
+
+    public void sum(int n) {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                this.matrix[i][j] += n;
+            }
+        }
+    }
+
+    public void pprint () {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < w; j++) {
+                System.out.printf(this.matrix[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int getW() {
+        return w;
+    }
+
+    public void setW(int w) {
+        this.w = w;
+    }
+
+    public int getH() {
+        return h;
+    }
+
+    public void setH(int h) {
+        this.h = h;
+    }
+}
