bd4d67ad4026b8a028a4434e7cde07d1298caeb9
cloud866 <dklein927@gmail.com>
2023-01-18 19:40:01 +0200

    add get set

--- задачки/Cat6Mod6/src/Cat.java
+++ задачки/Cat6Mod6/src/Cat.java
@@ -1,6 +1,6 @@
 public class Cat {
-    String name, owner;
-    int age;
+    private String name, owner;
+    private int age;
 
     public Cat(String name, int age, String owner) {
         this.name = name;
@@ -11,4 +11,32 @@ public class Cat {
     public String toString() {
         return "The cat " + name + " is already " + age + ". Her master " + owner + " takes care of her.";
     }
+
+    public void eat(){}
+    public void run(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
 }
